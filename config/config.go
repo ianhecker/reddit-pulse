4bdf89056201dda0ca2344406738cea004758ac8
ian hecker <ianhecker.dev@gmail.com>
2025-04-01 09:53:59 -0600

    Chore[config] Add config package

---
+++ config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ClientID     string
+	ClientSecret string
+	UserAgent    string
+	Username     string
+	Password     string
+}
+
+func NewConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("could not load env file: %w", err)
+	}
+
+	config := Config{}
+	err = config.GetStringFromENV(&config.ClientID, "CLIENT_ID")
+	if err != nil {
+		return nil, err
+	}
+
+	err = config.GetStringFromENV(&config.ClientSecret, "CLIENT_SECRET")
+	if err != nil {
+		return nil, err
+	}
+
+	err = config.GetStringFromENV(&config.UserAgent, "USER_AGENT")
+	if err != nil {
+		return nil, err
+	}
+
+	err = config.GetStringFromENV(&config.Username, "USER_NAME")
+	if err != nil {
+		return nil, err
+	}
+
+	err = config.GetStringFromENV(&config.Password, "PASSWORD")
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func (cfg *Config) GetStringFromENV(field *string, variableName string) error {
+	s := os.Getenv(variableName)
+	if s == "" {
+		return fmt.Errorf("%s was empty in env file", variableName)
+	}
+	*field = s
+	return nil
+}
